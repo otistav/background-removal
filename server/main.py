5d93241b9635162eab95cb948d08bf60ba4b3761
Sam Bocharov <sam@Sams-MacBook-Air.local>
2021-12-20 13:33:03 +0200

    server setup

--- server/main.py
+++ server/main.py
@@ -1,10 +1,10 @@
 from flask import Flask, jsonify, request
-from ml_service import main
+from ml_service.main import process
 app = Flask(__name__)
 
 @app.route('/image', methods=['POST'])
 def echo():
     params = jsonify(request.get_json(force=True))
 
-    main.process(params['input'], params['output'], 'u2net', 'bbd-fastrcnn', 'rtb-bnb')
+    process(params['input'], params['output'], 'u2net', 'bbd-fastrcnn', 'rtb-bnb')
     return jsonify({"jjj": "eee"})
