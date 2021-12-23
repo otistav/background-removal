4bd6d96f2246a0bcddaaae893c62588146ab56c2
Sam Bocharov <sam@Sams-MacBook-Air.local>
2021-12-23 14:23:38 +0200

    update field

--- server.py
+++ server.py
@@ -1,16 +1,16 @@
 import time
 from main import process
 from flask import Flask, jsonify, request
-# from pymongo import MongoClient
-# import pymongo
+from pymongo import MongoClient
+import pymongo
 
 import datetime
 app = Flask(__name__)
 
-# def get_database():
-#     CONNECTION_STRING = "mongodb://localhost:27017/"
-#     client = MongoClient(CONNECTION_STRING)
-#     return client['test']# from main import process
+def get_database():
+    CONNECTION_STRING = "mongodb://localhost:27017/"
+    client = MongoClient(CONNECTION_STRING)
+    return client['test']
 
 # db = get_database()
 # print([doc for doc in db['images'].find()])
@@ -44,6 +44,31 @@ def echo():
     params = request.json
     new_id = f"processed_{params['filename']}"
     process(f"../uploads/{params['filename']}", new_id, 'u2net', 'bbd-fastrcnn', 'rtb-bnb')
-    return jsonify({"jjj": "eee"})
+    db = get_database()
+    data = db['images'].find_one({"filename": params['filename']})
+    db['images'].update_one({
+      '_id': data['_id']
+    },{
+      '$set': {
+        'processed': True,
+      }
+    }, upsert=False)
+    return jsonify({"status": "ok"})
 
-app.run()
+# @app.route('/test', methods=['POST'])
+# def test():
+#     params = request.json
+#     db = get_database()
+#     print([doc for doc in db['images'].find()])
+#     data = db['images'].find_one({"filename": params['filename']})
+#     db['images'].update_one({
+#       '_id': data['_id']
+#     },{
+#       '$set': {
+#         'processed': True,
+#       }
+#     }, upsert=False)
+#     return 'ok'
+#     # return {"data": [doc for doc in db['images'].find()]}
+
+# app.run()
