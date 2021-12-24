b6926d01c0af89a82ee3711a00dd6da170c58911
Sam Bocharov <sam@Sams-MacBook-Air.local>
2021-12-24 03:49:18 +0200

    fix

--- server.py
+++ server.py
@@ -36,13 +36,12 @@ def manage_worker():
     db = get_database()
     cursor = db['images'].find({ "processed": False })
     print(f"CURSOR LENGTH {len(list(cursor))}", flush=True)
-    if len(list(cursor)) == 0:
-      return
     for doc in cursor:
       print(f"PROCESSING IMAGE {doc['filename']}", flush=True)
       manage_image(doc["filename"])
       print(f"DONE WITH IMAGE {doc['filename']}", flush=True)
-
+    if len(list(cursor)) == 0:
+      return
     # manage_worker()
 
 
